27f0970a11a63cb3ab0b86f5227a0c93a8858e41
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-12 11:54:47 +0200

    PrototypeFileWriter tests + implementation

--- src/main/kotlin/prototype/PrototypeFileWriter.kt
+++ src/main/kotlin/prototype/PrototypeFileWriter.kt
@@ -1,11 +1,17 @@
 package prototype
 
+import java.io.File
+
 class PrototypeFileWriter {
 
-    fun writeFile(filePath: String, file: String) {
+    fun writeFile(filePath: String, text: String) {
+        val file = File(filePath)
+        if (!file.exists()) file.createNewFile()
+        file.writeText(text)
     }
 
     fun readFile(filePath: String): String? {
-        return null
+        val file = File(filePath)
+        return if (file.exists()) file.readText() else null
     }
 }
--- src/test/kotlin/prototype/PrototypeFileWriterTest.kt
+++ src/test/kotlin/prototype/PrototypeFileWriterTest.kt
@@ -1,18 +1,59 @@
 package prototype
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 import org.junit.jupiter.api.Assertions.*
+import java.io.File
+import java.nio.file.Path
 
 internal class PrototypeFileWriterTest {
 
-    // todo figure out how to test writing files
+    companion object {
+        private val testPath = Path.of("test", "test.json").toAbsolutePath().toString()
 
-    @Test
-    fun writeFile() {
+        @AfterAll
+        @JvmStatic
+        internal fun afterAll() {
+            val file = File(testPath)
+            if (file.exists()) file.delete()
+        }
+
+        private const val TEST_STRING = "test"
+    }
+
+    lateinit var prototypeFileWriter: PrototypeFileWriter
+
+    @BeforeEach
+    internal fun setUp() {
+        prototypeFileWriter = PrototypeFileWriter()
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        val file = File(testPath)
+        if (file.exists()) file.delete()
     }
 
     @Test
-    fun readFile() {
+    fun `Writing file goes right`() {
+        prototypeFileWriter.writeFile(testPath, TEST_STRING)
+
+        val file = File(testPath)
+        assertEquals(TEST_STRING, file.readText())
+    }
+
+    @Test
+    internal fun `Reading file goes right`() {
+        val file = File(testPath)
+        file.writeText(TEST_STRING)
+
+        assertEquals(TEST_STRING, prototypeFileWriter.readFile(testPath))
+    }
+
+    @Test
+    internal fun `Reading non existing file`() {
+        assertFalse(File(testPath).exists())
+
+        assertNull(prototypeFileWriter.readFile(testPath))
     }
 }
