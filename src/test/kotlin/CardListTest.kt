4edd9f30da55952ebe390e407fc3634336e1d209
Rickard <rickard.andersson98@gmail.com>
2019-10-12 11:33:13 +0200

    v1.0 of removeCardTest

--- src/test/kotlin/CardListTest.kt
+++ src/test/kotlin/CardListTest.kt
@@ -1,11 +1,45 @@
+import Models.Deck
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import java.lang.Exception
+import java.lang.RuntimeException
 
 internal class CardListTest {
 
     @Test
     fun removeCardTest() {
-        
+        var pigMonster: Card = Monster("Pig")
+        var rabbitMonster: Card = Monster("Rabbit")
+        var deck: Deck = Deck(false, arrayListOf(pigMonster))
+
+        var removedCard: Card = Monster()
+
+        try {
+            removedCard = deck.removeCard(pigMonster)
+        } catch (error: Exception) {
+            assertTrue(false, "Something went wrong when trying to remove a card")
+        }
+
+        assertEquals(pigMonster, removedCard, "The removed card doesn't match the card we wanted to remove")
+        assertEquals(0, deck.cards.size, "The card did not get removed, a card still exists in the deck")
+        assertEquals(true, deck.empty, "The boolean empty should be set to true because no cards exists")
+
+        deck = Deck(false, arrayListOf(rabbitMonster, pigMonster))
+
+        try {
+            removedCard = deck.removeCard(rabbitMonster)
+        } catch (error: Exception) {
+            assertTrue(false, "Something went wrong when trying to remove a card")
+        }
+
+        assertEquals(rabbitMonster, removedCard, "The removed card doesn't match the card we wanted to remove")
+        assertEquals(1, deck.cards.size, "The card did not get removed, card array size is not correct")
+        assertEquals(pigMonster, deck.cards[0],"The card that should exist is not the on existing")
+
+        try {
+            deck.removeCard(rabbitMonster)
+            assertTrue(false, "Trying to remove a card that does not exist,should throw runtimeException")
+        } catch (error: RuntimeException) {}
     }
 }
