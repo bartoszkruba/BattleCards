c5105b260afa84d9a8480bca4ace397d8aa0fae3
RickardA <rickard.andersson98@gmail.com>
2019-10-14 14:44:42 +0200

    Revert "changed init so if you pass a ArrayList to constructor it does a deep clone of the ArrayList, test passed"
    This reverts commit eb7e0a69541d5d04ba78c6c80669e0909d74cff4.

--- src/main/kotlin/Models/CardList.kt
+++ src/main/kotlin/Models/CardList.kt
@@ -8,7 +8,7 @@ abstract class CardList(var empty: Boolean, cards: ArrayList<Card>) {
     var cards: ArrayList<Card> = ArrayList()
 
     init {
-        this.cards = cards.map{Utils.clone(it) as Card} as ArrayList<Card>
+        this.cards = ArrayList(cards)
     }
 
     fun cardsInList(): ArrayList<Card> {
