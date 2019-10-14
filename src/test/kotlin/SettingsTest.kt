b380f559df1617fa79614c4d437cfa714414e393
Johan <johanwiren1@gmail.com>
2019-10-14 14:50:02 +0200

    Test if all peoperties are consts, and no other property allowed

--- src/test/kotlin/SettingsTest.kt
+++ src/test/kotlin/SettingsTest.kt
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.memberProperties
 
 internal class SettingsTest {
 
     @Test
     internal fun settingsConstructorTest() {
         assertTrue(Settings::class.isAbstract, "Can't create instance from abstract class")
+
+        assertEquals(0, Settings::class.memberProperties.toList().size)
+
+        Settings::class.companionObject!!.memberProperties.forEach {
+           assertTrue(it.isConst, "Property $it must be a constant")
+        }
     }
 }
