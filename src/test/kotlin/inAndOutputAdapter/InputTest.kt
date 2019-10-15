037295b1714232f9fdec489bc8156571516c4525
Rami <rami.albadri77@gmail.com>
2019-10-15 20:46:28 +0200

    more testcode for deckNameInput if user enter number of the deck name in the list

--- src/test/kotlin/inAndOutputAdapter/InputTest.kt
+++ src/test/kotlin/inAndOutputAdapter/InputTest.kt
@@ -8,6 +8,14 @@ internal class InputTest {
 
     @Test
     fun userNameInput() {
+        var userInput = Input()
+
+        assertFalse(userInput.userNameValidation(null))
+        assertTrue(userInput.userNameValidation("name"))
+        assertFalse(userInput.userNameValidation(""))
+        assertFalse(userInput.userNameValidation("1234"))
+        assertFalse(userInput.userNameValidation("name77"))
+        assertFalse(userInput.userNameValidation("name more than nine characters"))
     }
 
     @Test
@@ -18,23 +26,8 @@ internal class InputTest {
         assertTrue(userInput.deckNameInput(decks.get(0)))
         assertTrue(userInput.deckNameInput(decks.get(3)))
         assertFalse(userInput.deckNameInput("deck name"))
+        assertTrue(userInput.deckNameInput("1"))
+        assertFalse(userInput.userNameInput("7"))
         assertFalse(userInput.deckNameInput(null))
-    }
-
-    @Test
-    fun userNameValidation() {
-        var userInput = Input()
-
-        assertFalse(userInput.userNameValidation(null))
-
-        assertTrue(userInput.userNameValidation("name"))
-
-        assertFalse(userInput.userNameValidation(""))
-
-        assertFalse(userInput.userNameValidation("1234"))
-
-        assertFalse(userInput.userNameValidation("name77"))
-
-        assertFalse(userInput.userNameValidation("name more than nine characters"))
     }
 }
