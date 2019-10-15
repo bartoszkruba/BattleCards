251a8341be4b03c5732bccec7cc9d424cd7db359
Johan <johanwiren1@gmail.com>
2019-10-15 09:09:37 +0200

    Don't allow functions in Settings class

--- src/test/kotlin/SettingsTest.kt
+++ src/test/kotlin/SettingsTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.memberProperties
 
 internal class SettingsTest {
@@ -10,6 +11,8 @@ internal class SettingsTest {
         assertTrue(Settings::class.isAbstract, "Can't create instance from abstract class")
 
         assertEquals(0, Settings::class.memberProperties.toList().size, "Only constants in companion object allowed")
+        assertEquals(3, Settings::class.memberFunctions.toList().size, "Only constants in companion object allowed")
+        assertEquals(3, Settings::class.companionObject!!.memberFunctions.toList().size, "Only constants in companion object allowed")
 
         // Tests all companion properties
         Settings::class.companionObject!!.memberProperties.forEach {
