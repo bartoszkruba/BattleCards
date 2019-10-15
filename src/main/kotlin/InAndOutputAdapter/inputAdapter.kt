6f238a09aefd30fe9025e7cbef90e6df80983f4b
Rami <rami.albadri77@gmail.com>
2019-10-15 14:08:08 +0200

    inputAdapter class with nameInput and deckName methodes

---
+++ src/main/kotlin/InAndOutputAdapter/inputAdapter.kt
@@ -0,0 +1,11 @@
+package InAndOutputAdapter
+
+class inputAdapter() {
+    fun userNameInput(name: String?): Unit? {
+        return null
+    }
+
+    fun deckNameInput(deckName: String?): Unit? {
+        return null
+    }
+}
