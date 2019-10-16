a91dddc69a7184a0728f974ba0461f629c862455
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-16 17:42:19 +0200

    Implemented methods in deck

--- src/main/kotlin/models/Deck.kt
+++ src/main/kotlin/models/Deck.kt
@@ -6,12 +6,14 @@ class Deck(
     cards: ArrayList<Card> = ArrayList()
 ) : CardList(cards.size == 0, cards, Settings.DECK_SIZE) {
 
-    fun shuffleDeck() {
-        
-    }
+    fun shuffleDeck() = cards.shuffle()
 
-    fun drawCard(): Card? {
-        return null
+    fun drawCard(): Card? = when (cards.size) {
+        0 -> null
+        else -> {
+            if (cards.size - 1 == 0) empty = true
+            cards.removeAt(0)
+        }
     }
 
 }
