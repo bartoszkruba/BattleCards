346ae2ddb5debafa6a47715912b3c78266978e49
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-16 18:41:45 +0200

    DeckFactory passes all tests

--- src/main/kotlin/factory/DeckFactory.kt
+++ src/main/kotlin/factory/DeckFactory.kt
@@ -6,7 +6,11 @@ import prototype.DeckPrototype
 class DeckFactory {
     companion object {
         fun createDeck(deck: DeckPrototype): Deck {
-            return Deck()
+            val newDeck = Deck()
+            for (entry in deck.cardsCopy().entries) repeat(entry.value) {
+                newDeck.addCard(CardFactory.createCard(entry.key))
+            }
+            return newDeck
         }
     }
 
