cdae0c2363d716717a4471285f395fb50adbbec9
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-16 17:06:19 +0200

    implemented tests for deck

--- src/main/kotlin/models/Deck.kt
+++ src/main/kotlin/models/Deck.kt
@@ -1,16 +1,17 @@
 package models
 
 import Card
-import Monster
 
 class Deck(
     cards: ArrayList<Card> = ArrayList()
 ) : CardList(cards.size == 0, cards, Settings.DECK_SIZE) {
 
-    fun shuffleDeck() {}
+    fun shuffleDeck() {
+        
+    }
 
-    fun drawCard(): Card {
-        return Monster()
+    fun drawCard(): Card? {
+        return null
     }
 
 }
