9c4a7280bffd0a5865a5048961f27f999a4d3d58
Rami <rami.albadri77@gmail.com>
2019-10-17 15:26:55 +0200

    pretend print the field with cards for now and then a list to choose from

--- src/main/kotlin/inAndOutputAdapter/GameController.kt
+++ src/main/kotlin/inAndOutputAdapter/GameController.kt
@@ -1,33 +1,49 @@
 package inAndOutputAdapter
 
+import Game
+import factory.DeckFactory
+import prototype.CardLoader
+
 class GameController {
 
     fun printMainScreen() {
-        //Output.printWelcome()
-        //Output.printEnterName(1)
-        print("Add player 1 name: ")
-        var playerNameOne = readLine()
-        var validPlayerOneName = Input.readName(playerNameOne!!)
+        OutputAdapter.printWelcome()
+        OutputAdapter.printEnterName(1)
+        var playerOneName = readLine()
+        var validPlayerOneName = Input.readName(playerOneName!!)
         while(validPlayerOneName == null){
-            //Output.printIlegalInput()
-            print("Wrong input of name \nTry again: ")
-            playerNameOne = readLine()
-            if(Input.readName(playerNameOne!!) != null){
-                validPlayerOneName = playerNameOne
+            OutputAdapter.illegalInputInfo()
+            playerOneName = readLine()
+            if(Input.readName(playerOneName!!) != null){
+                validPlayerOneName = playerOneName
             }
         }
 
-        print("Add player 2 name: ")
-        var playerNameTwo = readLine()
-        var validPlayerTwoName = Input.readName(playerNameTwo!!)
+        OutputAdapter.printEnterName(2)
+        var playerTwoName = readLine()
+        var validPlayerTwoName = Input.readName(playerTwoName!!)
         while(validPlayerTwoName == null){
-            //Output.printIlegalInput()
-            print("Wrong input of name \nTry again: ")
-            playerNameTwo = readLine()
-            if(Input.readName(playerNameTwo!!) != null){
-                validPlayerTwoName = playerNameTwo
+            OutputAdapter.illegalInputInfo()
+            playerTwoName = readLine()
+            if(Input.readName(playerTwoName!!) != null){
+                validPlayerTwoName = playerTwoName
             }
         }
+
+        val cardLoader = CardLoader()
+
+        val deckPrototype = cardLoader.loadDeck("test")
+        val playerOneDeck = DeckFactory.createDeck(deckPrototype)
+        val playerTwoDeck = DeckFactory.createDeck(deckPrototype)
+
+        var game = Game(playerOneDeck, playerTwoDeck, playerOneName!!, playerTwoName!!)
+        OutputAdapter.printBoard(game)
+
+        val gameOptions= mapOf( 1 to "Draw", 2 to "Put",3 to "Attack", 4 to "Pass")
+        OutputAdapter.printGameOptions(gameOptions)
+
+
+
 
     }
 
