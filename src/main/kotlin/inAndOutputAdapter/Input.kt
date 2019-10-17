02e3c32d20e11e165c98e0a017eb0d634856a729
Rami <rami.albadri77@gmail.com>
2019-10-17 14:54:18 +0200

    unnecessary staff

--- src/main/kotlin/inAndOutputAdapter/Input.kt
+++ src/main/kotlin/inAndOutputAdapter/Input.kt
@@ -10,25 +10,6 @@ class Input() {
         fun readName(name: String): String? {
             var validUserName = userNameValidation(name)
             if(validUserName) return name else return null
-            /*if (playerNumber in 1..2) {
-                var validUserName = userNameValidation(name)
-                if (validUserName) {
-                    if (playerNumber == 1) playerOne.name = name else playerTwo.name = name
-                }
-                return validUserName
-            }
-            return false*/
-        }
-
-        fun deckChoice(deckInput: String, playerNumber: Int): Boolean {
-            if (playerNumber in 1..2) {
-                var validDeckName = deckInputValidation(deckInput)
-                if (validDeckName) {
-                    //if (playerNumber == 1) playerOne.deck = Deck()!! else playerTwo.deck = Deck()!!
-                }
-                return validDeckName
-            }
-            return false
         }
 
         fun menu(choice: String): String? {
@@ -44,22 +25,6 @@ class Input() {
         fun userNameValidation(name: String): Boolean {
             val regex = Regex("^[a-zA-Z]{1,9}")
             return name.length in 1..9 && regex.matches(name)
-
-        }
-
-        fun deckInputValidation(name: String): Boolean {
-            val regex = Regex("^\\d")
-            val decks = listOf("one", "two", "three", "four")
-            if (regex.matches(name)) {
-                for ((index) in decks.withIndex()) {
-                    if (index == name.toInt() - 1) return true
-                }
-            } else {
-                for ((index, value) in decks.withIndex()) {
-                    if (value == name) return true
-                }
-            }
-            return false
         }
     }
 }
