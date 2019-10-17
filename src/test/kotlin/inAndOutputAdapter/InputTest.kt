3e48a4809bdaa2404757277c2e0d635f9f85ab93
Rami <rami.albadri77@gmail.com>
2019-10-17 15:59:37 +0200

    testcode for readGameOptions method

--- src/test/kotlin/inAndOutputAdapter/InputTest.kt
+++ src/test/kotlin/inAndOutputAdapter/InputTest.kt
@@ -18,28 +18,46 @@ internal class InputTest {
     }
 
     @Test
-    fun deckChoice() {
-        var userInput = Input()
-        val decks = listOf("one", "two", "three", "four")
+    fun readGameOptions(){
+        val gameOptions= mapOf( 1 to "Draw", 2 to "Put",3 to "Attack", 4 to "Pass")
 
-        assertTrue(userInput.deckChoice(decks.get(0), 1))
-        assertTrue(userInput.deckChoice(decks.get(3), 2))
-        assertFalse(userInput.deckChoice("random deck name", 2))
-        assertFalse(userInput.deckChoice("two", 0))
-        assertTrue(userInput.deckChoice("1", 1))
-        assertFalse(userInput.deckChoice("7", 2))
+        assertNotNull(Input.readGameOptions("1", gameOptions))
+        assertNotNull(Input.readGameOptions("Draw", gameOptions))
+        assertNotNull(Input.readGameOptions("draw", gameOptions))
+
+        assertNull(Input.readGameOptions("someOption", gameOptions))
+        assertNull(Input.readGameOptions("5", gameOptions))
+        assertNull(Input.readGameOptions("0", gameOptions))
+        assertNull(Input.readGameOptions("-2", gameOptions))
+
+
     }
+   /*
+   @Test
+   fun deckChoice() {
+       var userInput = Input()
+       val decks = listOf("one", "two", "three", "four")
 
-    @Test
-    fun menu() {
-        var userInput = Input()
+       /*astTrue(userInput.deckChoice(decks.get(0), 1))
+       assertTrue(userInput.deckChoice(decks.get(3), 2))
+       assertFalse(userInput.deckChoice("random deck name", 2))
+       assertFalse(userInput.deckChoice("two", 0))
+       assertTrue(userInput.deckChoice("1", 1))
+       assertFalse(userInput.deckChoice("7", 2))
+   }
 
-        assertNotNull(userInput.menu("2"))
-        assertNotNull(userInput.menu("Main Menu"))
-        assertNotNull(userInput.menu("1"))
-        assertNotNull(userInput.menu("Sub Menu"))
-        assertEquals("Sub Menu", userInput.menu("Sub Menu"))
-        assertNull(userInput.menu("something"))
-        assertNull(userInput.menu("3"))
-    }
+   @Test
+   fun menu() {
+       var userInput = Input()
+
+       assertNotNull(userInput.menu("2"))
+       assertNotNull(userInput.menu("Main Menu"))
+       assertNotNull(userInput.menu("1"))
+       assertNotNull(userInput.menu("Sub Menu"))
+       assertEquals("Sub Menu", userInput.menu("Sub Menu"))
+       assertNull(userInput.menu("something"))
+       assertNull(userInput.menu("3"))
+   }
+   */
+    */
 }
