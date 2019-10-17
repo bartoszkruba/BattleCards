f76375fa893d7d7dbea7e459d5a67f0c30b11aa2
Rami <rami.albadri77@gmail.com>
2019-10-17 12:02:41 +0200

    the players has to enter their names to continue

--- src/main/kotlin/inAndOutputAdapter/GameController.kt
+++ src/main/kotlin/inAndOutputAdapter/GameController.kt
@@ -3,8 +3,32 @@ package inAndOutputAdapter
 class GameController {
 
     fun printMainScreen() {
-        var userName = readLine()
-        Input.readName(userName!!)
+        //Output.printWelcome()
+        //Output.printEnterName(1)
+        print("Add player 1 name: ")
+        var playerNameOne = readLine()
+        var validPlayerOneName = Input.readName(playerNameOne!!)
+        while(validPlayerOneName == null){
+            //Output.printIlegalInput()
+            print("Wrong input of name \nTry again: ")
+            playerNameOne = readLine()
+            if(Input.readName(playerNameOne!!) != null){
+                validPlayerOneName = playerNameOne
+            }
+        }
+
+        print("Add player 2 name: ")
+        var playerNameTwo = readLine()
+        var validPlayerTwoName = Input.readName(playerNameTwo!!)
+        while(validPlayerTwoName == null){
+            //Output.printIlegalInput()
+            print("Wrong input of name \nTry again: ")
+            playerNameTwo = readLine()
+            if(Input.readName(playerNameTwo!!) != null){
+                validPlayerTwoName = playerNameTwo
+            }
+        }
+
     }
 
 }
