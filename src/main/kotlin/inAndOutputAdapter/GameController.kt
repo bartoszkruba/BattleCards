ab44530d3717b12a8e45b10143c269303a3920be
Rami <rami.albadri77@gmail.com>
2019-10-18 11:35:51 +0200

    print out the card when the choice is "Draw"

--- src/main/kotlin/inAndOutputAdapter/GameController.kt
+++ src/main/kotlin/inAndOutputAdapter/GameController.kt
@@ -8,6 +8,7 @@ import prototype.CardLoader
 class GameController {
     private var playerOneName: String? = null
     private var playerTwoName: String? = null
+    lateinit var game:Game
 
     fun printMainScreen() {
         //OutputAdapter.printWelcome()
@@ -15,6 +16,7 @@ class GameController {
         this.playerTwoName = userNameInput(2)
         var playerDecks = printGameBoard()
         val chosenOption = gameOptions()
+        doTheChoice(chosenOption, playerDecks)
 
     }
 
@@ -25,14 +27,21 @@ class GameController {
         val playerOneDeck = DeckFactory.createDeck(deckPrototype)
         val playerTwoDeck = DeckFactory.createDeck(deckPrototype)
 
-        val game = Game(playerOneDeck, playerTwoDeck, this.playerOneName!!, this.playerTwoName!!)
+        this.game = Game(playerOneDeck, playerTwoDeck, this.playerOneName!!, this.playerTwoName!!)
         OutputAdapter.printBoard(game)
         return Pair(playerOneDeck, playerTwoDeck)
     }
 
+    private fun doTheChoice(option: String?, playersDecks: Pair<Deck, Deck>) {
+        val whiteTurn = this.game.turn % 2 != 0
+        when(option){
+            "1", "draw" -> OutputAdapter.printDrawCardFromDeck(playersDecks.first.drawCard()!!)
+            //"2", "put" -> if(whiteTurn) OutputAdapter.
+        }
+    }
+
     private fun userNameInput(playerNumber:Int): String? {
         OutputAdapter.printEnterName(playerNumber)
-
         var playerName = readLine()
         val validPlayerName = Input.readName(playerName!!)
         while(validPlayerName == null){
