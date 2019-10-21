54f9a8a369e62f212d169fb91372f494b212f799
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-21 13:58:35 +0200

    Added test for DeckFactory creating deck with Spells

--- src/test/kotlin/factory/DeckFactoryTest.kt
+++ src/test/kotlin/factory/DeckFactoryTest.kt
@@ -1,11 +1,12 @@
 package factory
 
 import Monster
-import models.Deck
+import Spell
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import prototype.DeckPrototype
 import prototype.MonsterPrototype
+import prototype.SpellPrototype
 
 internal class DeckFactoryTest {
 
@@ -24,10 +25,10 @@ internal class DeckFactoryTest {
         private const val ID_FOUR = 4
     }
 
-    private val MONSTER_NAME_ONE: String
-    private val MONSTER_NAME_TWO: String
-    private val MONSTER_NAME_THREE: String
-    private val MONSTER_NAME_FOUR: String
+    private val NAME_ONE: String
+    private val NAME_TWO: String
+    private val NAME_THREE: String
+    private val NAME_FOUR: String
 
     private val DECK_NAME_ONE: String
 
@@ -46,20 +47,20 @@ internal class DeckFactoryTest {
             sbE.append("E")
         }
 
-        MONSTER_NAME_ONE = sbA.toString()
-        MONSTER_NAME_TWO = sbB.toString()
-        MONSTER_NAME_THREE = sbC.toString()
-        MONSTER_NAME_FOUR = sbD.toString()
+        NAME_ONE = sbA.toString()
+        NAME_TWO = sbB.toString()
+        NAME_THREE = sbC.toString()
+        NAME_FOUR = sbD.toString()
 
         DECK_NAME_ONE = sbE.toString()
     }
 
     @Test
-    internal fun `createDeck() test`() {
-        val prototypeOne = MonsterPrototype(ID_ONE, MONSTER_NAME_ONE, MAX_HEALTH, MAX_ATTACK)
-        val prototypeTwo = MonsterPrototype(ID_TWO, MONSTER_NAME_TWO, MIN_HEALTH, MIN_ATTACK)
-        val prototypeThree = MonsterPrototype(ID_THREE, MONSTER_NAME_THREE, MAX_HEALTH, MIN_ATTACK)
-        val prototypeFour = MonsterPrototype(ID_FOUR, MONSTER_NAME_FOUR, MIN_HEALTH, MAX_ATTACK)
+    internal fun `createDeck() test, with monsters`() {
+        val prototypeOne = MonsterPrototype(ID_ONE, NAME_ONE, MAX_HEALTH, MAX_ATTACK)
+        val prototypeTwo = MonsterPrototype(ID_TWO, NAME_TWO, MIN_HEALTH, MIN_ATTACK)
+        val prototypeThree = MonsterPrototype(ID_THREE, NAME_THREE, MAX_HEALTH, MIN_ATTACK)
+        val prototypeFour = MonsterPrototype(ID_FOUR, NAME_FOUR, MIN_HEALTH, MAX_ATTACK)
 
         val deckPrototype = DeckPrototype(DECK_NAME_ONE)
 
@@ -77,24 +78,60 @@ internal class DeckFactoryTest {
         val cardThree = cards[2] as Monster
         val cardFour = cards[3] as Monster
 
-        assertEquals(MONSTER_NAME_ONE, cardOne.name)
+        assertEquals(NAME_ONE, cardOne.name)
         assertEquals(MAX_HEALTH, cardOne.health)
         assertEquals(MAX_ATTACK, cardOne.attack)
         assertEquals(CardType.MONSTER, cardOne.type)
 
-        assertEquals(MONSTER_NAME_TWO, cardTwo.name)
+        assertEquals(NAME_TWO, cardTwo.name)
         assertEquals(MIN_HEALTH, cardTwo.health)
         assertEquals(MIN_ATTACK, cardTwo.attack)
         assertEquals(CardType.MONSTER, cardTwo.type)
 
-        assertEquals(MONSTER_NAME_THREE, cardThree.name)
+        assertEquals(NAME_THREE, cardThree.name)
         assertEquals(MAX_HEALTH, cardThree.health)
         assertEquals(MIN_ATTACK, cardThree.attack)
         assertEquals(CardType.MONSTER, cardThree.type)
 
-        assertEquals(MONSTER_NAME_FOUR, cardFour.name)
+        assertEquals(NAME_FOUR, cardFour.name)
         assertEquals(MIN_HEALTH, cardFour.health)
         assertEquals(MAX_ATTACK, cardFour.attack)
         assertEquals(CardType.MONSTER, cardFour.type)
+    }
+
+    @Test
+    internal fun `createCard() test, with spells`() {
+        val prototypeOne = SpellPrototype(ID_ONE, NAME_ONE)
+        val prototypeTwo = SpellPrototype(ID_TWO, NAME_TWO)
+        val prototypeThree = SpellPrototype(ID_THREE, NAME_THREE)
+        val prototypeFour = SpellPrototype(ID_FOUR, NAME_FOUR)
+
+        val deckPrototype = DeckPrototype(DECK_NAME_ONE)
+
+        deckPrototype.addCard(prototypeOne)
+        deckPrototype.addCard(prototypeTwo)
+        deckPrototype.addCard(prototypeThree)
+        deckPrototype.addCard(prototypeFour)
+
+        val deck = DeckFactory.createDeck(deckPrototype)
+
+        val cards = deck.cardsInList()
+
+        val cardOne = cards[0] as Spell
+        val cardTwo = cards[1] as Spell
+        val cardThree = cards[2] as Spell
+        val cardFour = cards[3] as Spell
+
+        assertEquals(NAME_ONE, cardOne.name)
+        assertEquals(CardType.SPEll, cardOne.type)
+
+        assertEquals(NAME_TWO, cardTwo.name)
+        assertEquals(CardType.SPEll, cardTwo.type)
+
+        assertEquals(NAME_THREE, cardThree.name)
+        assertEquals(CardType.SPEll, cardThree.type)
+
+        assertEquals(NAME_FOUR, cardFour.name)
+        assertEquals(CardType.SPEll, cardFour.type)
     }
 }
